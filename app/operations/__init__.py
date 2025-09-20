983f214d5483de54c37bcea0ffefdfd7045bd561
Maxiel De Jesus <mid6@njit.edu>
2025-09-20 15:32:13 -0400

    encapsulated opertion methods inside of a class called Operations.

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,13 +1,22 @@
-def addition(a: float, b:float) -> float:
-    return a + b
+class Operations:
 
-def subtraction(a: float, b:float) -> float:
-    return a - b
+    # Static methods allow us to use these methods without needing to create an instance of the class
+    # This is an example of encapsulation. Methods are encapsulated within a class.
+    
+    @staticmethod
+    def addition(a: float, b:float) -> float:
+        return a + b
 
-def multiplication(a: float, b:float) -> float:
-    return a * b
+    @staticmethod
+    def subtraction(a: float, b:float) -> float:
+        return a - b
 
-def division(a: float, b:float) -> float:
-    if b == 0:
-        raise ValueError("Division by zero is not allowed.") 
-    return a / b  
+    @staticmethod
+    def multiplication(a: float, b:float) -> float:
+        return a * b
+
+    @staticmethod
+    def division(a: float, b:float) -> float:
+        if b == 0:
+            raise ValueError("Division by zero is not allowed.") 
+        return a / b  
